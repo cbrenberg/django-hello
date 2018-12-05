745397d723615497e45a66fc4fff4d1d71c5903a
Christopher Brenberg <cbrenberg@gmail.com>
2018-12-04 21:43:07 -0600

    add style and checkboxes

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+
 from .models import Todo
 
 # Create your views here.
@@ -8,9 +8,8 @@ from .models import Todo
 def index(request):
     todo_list = Todo.objects.order_by('-due_date')
     output = ', '.join([t.todo_item for t in todo_list])
-    template = loader.get_template('hello/index.html')
     context = {
         "todo_list": todo_list,
     }
     print(todo_list, output)
-    return HttpResponse(template.render(context, request))
+    return render(request, 'hello/index.html', context)
