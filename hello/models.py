dc143fbf41f4f85f6c627c354db4994ca891aaef
Christopher Brenberg <cbrenberg@gmail.com>
2018-12-04 20:53:07 -0600

    initialize database, make first view

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Todo(models.Model):
+    todo_item = models.CharField(max_length=250)
+    is_complete = models.BooleanField(default=False)
+    due_date = models.DateField('due date', default=timezone.now)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.todo_item
--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Todo
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the hello index.")
+    todo_list = Todo.objects.order_by('-due_date')
+    output = ', '.join([t.todo_item for t in todo_list])
+    template = loader.get_template('hello/index.html')
+    context = {
+        "todo_list": todo_list,
+    }
+    print(todo_list, output)
+    return HttpResponse(template.render(context, request))
